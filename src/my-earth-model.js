f75c635883219713c42087cd558b2a3e77c334b1
jjcadenza5-arch <jjcadenza5@gmail.com>
2026-09-19 12:36:54 +0700

    ERN My Earth: include recently watched windows

--- src/my-earth-model.js
+++ src/my-earth-model.js
@@ -1,1 +1,21 @@
-import { loadFavorites } from "./favorites.js";import { loadFavoritePlaces } from "./place-favorites.js";import { loadRecentPlaces } from "./recent-places.js";import { discoverableSource } from "./discovery-eligibility.js";export function buildMyEarth({sources,places}){const sourceFav=loadFavorites(),placeFav=loadFavoritePlaces(),recent=loadRecentPlaces(),placeMap=new Map((places||[]).map(p=>[p.id,p])),sourceMap=new Map((sources||[]).map(s=>[s.id,s]));const favoriteWindows=[...sourceFav].map(id=>sourceMap.get(id)).filter(Boolean),availableFavoriteWindows=favoriteWindows.filter(discoverableSource);return{favoritePlaces:[...placeFav].map(id=>placeMap.get(id)).filter(Boolean),favoriteWindows,availableFavoriteWindows,unavailableFavoriteWindows:favoriteWindows.filter(s=>!discoverableSource(s)),recentPlaces:recent.map(id=>placeMap.get(id)).filter(Boolean)}}export function myEarthCount(model){return model.favoritePlaces.length+model.favoriteWindows.length}
+import { loadFavorites } from "./favorites.js";
+import { loadFavoritePlaces } from "./place-favorites.js";
+import { loadRecentPlaces } from "./recent-places.js";
+import { loadRecentWindows } from "./recent-windows.js";
+import { discoverableSource } from "./discovery-eligibility.js";
+
+export function buildMyEarth({sources,places}){
+ const sourceFav=loadFavorites(),placeFav=loadFavoritePlaces(),recent=loadRecentPlaces(),recentWindows=loadRecentWindows(),placeMap=new Map((places||[]).map(p=>[p.id,p])),sourceMap=new Map((sources||[]).map(s=>[s.id,s]));
+ const favoriteWindows=[...sourceFav].map(id=>sourceMap.get(id)).filter(Boolean),availableFavoriteWindows=favoriteWindows.filter(discoverableSource);
+ const visitedWindows=recentWindows.map(id=>sourceMap.get(id)).filter(Boolean);
+ return{
+  favoritePlaces:[...placeFav].map(id=>placeMap.get(id)).filter(Boolean),
+  favoriteWindows,
+  availableFavoriteWindows,
+  unavailableFavoriteWindows:favoriteWindows.filter(s=>!discoverableSource(s)),
+  recentPlaces:recent.map(id=>placeMap.get(id)).filter(Boolean),
+  recentWindows:visitedWindows,
+  availableRecentWindows:visitedWindows.filter(discoverableSource)
+ };
+}
+export function myEarthCount(model){return model.favoritePlaces.length+model.favoriteWindows.length}
