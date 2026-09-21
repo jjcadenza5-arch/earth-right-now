5a452f0b2473d2c5f6b61e41a9c166affef1aa14
jjcadenza5-arch <jjcadenza5@gmail.com>
2026-09-21 18:07:45 +0700

    Keep small-place card copy evidence based

--- src/small-place-context.js
+++ src/small-place-context.js
@@ -1,5 +1,5 @@
 import {smallPlaceSignals} from "./small-place-discovery.js";
 export function smallPlaceContext(place={}){
  const sources=place.sources?.length?place.sources:[place],worth=sources.some(s=>smallPlaceSignals(s).discoveryWorth);
- return worth?"A lesser-known place worth a look":""; 
+ return worth?"A local place worth discovering":"";
 }
